f6d13577181b2b49e2270c134640546103f0b664
mihomatsui <mihomatsui.24@gmail.com>
2021-01-06 17:52:59 +0900

    Characterクラスの説明を明記

--- main.rb
+++ main.rb
@@ -5,4 +5,18 @@ class Monster < Character
 end
 
 class Character
+  def initialize()
+    @name
+    @hp
+    @offense
+    @defense
 end
+
+# クラス:Character
+# 情報:name,hp,offense(攻め),defense(防御)
+# 役割：-(なし。存在のみ)
+
+# クラス:Brave
+
+
+# クラス:Monster
