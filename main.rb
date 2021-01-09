46e2addab12185533daf34b5f5d7b4aae49330db
mihomatsui <mihomatsui.24@gmail.com>
2021-01-09 11:26:42 +0900

    状況整理のためコメントを追加

--- main.rb
+++ main.rb
@@ -11,19 +11,31 @@ end
 class Brave < Character
   def attack(monster)
     # 勇者がモンスターに攻撃する
-    puts "#{monster.name} があらわれた!"
-    puts "ゆうしゃ の攻撃！"
-    # 勇者がモンスターに攻撃する
     # モンスターのHPを減らす
+    monster.damage = brave[:offense] - monster[:defense] / 2
+    monster.hp =- monster.damage
+
+    puts <<~TEXT
+    #{monster.name} があらわれた!
+    ゆうしゃ の攻撃！
+    TEXT
+
     while true
-      puts "#{monster.name}に#{}"
+      puts "#{monster.name}に#{monster.damage} のダメージを与えた！"
     end
   end
 end
 
 class Monster < Character
   def attack(brave)
+    # モンスターが勇者に攻撃する
+    # 勇者のHPを減らす
+    brave.damage = monster.offense - brave.defense / 2
+    brave.hp =- brave.damage
+
+    puts "#{monster.name} の攻撃！"
     while true
+      puts "ゆうしゃ は #{brave.damage} のダメージを受けた！"
     end
   end
 end
@@ -42,10 +54,12 @@ end
 # 情報:Character
 # 役割：勇者に攻撃する、勇者からダメージを受ける
 
+# 実体を生成する部分、呼び出し部分 -------------------
 #インスタンスを生成する
-brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
-monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
+  brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
+  monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
 
-
-brave.attack(monster)
-monster.attack(brave)
+# 勇者がモンスターを攻撃する
+  brave.attack(monster)
+# モンスターが勇者を攻撃する
+  monster.attack(brave)
