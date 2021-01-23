1ebfbf05367646fbdcdf0ca0976257ebb3c32a46
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 00:26:11 +0900

    monsterクラスにメソッド2つ追加

--- main.rb
+++ main.rb
@@ -32,14 +32,25 @@ end
 class Monster < Character
   def attack(brave)
     # モンスターが勇者に攻撃する
-    # 勇者が受けるダメージの計算
-    damage = @offense - brave.defense / 2
-    brave.hp -= damage
-    brave.hp = 0 if brave.hp < 0
-    # メッセージの追記
     puts "#{@name} の攻撃!"
-    puts "#{brave.name} に #{damage} のダメージを与えた!"
+    # 勇者が受けるダメージの計算
+    damage = calculate_damage(brave)
+    cause_damage(target: brave, damage: damage)
   end
+
+  private
+    def calculate_damage(target)
+      @offense - target.defense / 2
+    end
+
+    def cause_damage(**params)
+      damage = params[:damage]
+      target = params[:target]
+
+      target.hp -= damage
+      target.hp = 0 if target.hp < 0
+      puts "#{target.name} に #{damage} のダメージを与えた!"
+    end
 end
 
 
