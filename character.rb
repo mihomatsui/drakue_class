d49a09e29e6e88115455d6bb6e06e08bb06683c9
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 09:47:31 +0900

    親クラスとしてcharacterクラスを設定

---
+++ character.rb
@@ -0,0 +1,10 @@
+class Character
+  attr_accessor :hp
+  attr_reader :name, :offense, :defense
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
