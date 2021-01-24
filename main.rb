cf01bd0c824adf99959b243d2cd35264b9b1e695
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 10:06:56 +0900

    各クラスの分割とrequireの追記

--- main.rb
+++ main.rb
@@ -1,57 +1,5 @@
-require "./character"
-
-
-class Brave < Character
-
-  def attack(monster)
-    # 勇者がモンスターに攻撃する
-    puts "#{@name} の攻撃!"
-    #モンスターが受けるダメージの計算
-    damage = calculate_damage(monster)
-
-    # モンスターのHPから計算したダメージを引く
-    cause_damage(target: monster,damage: damage)
-
-  end
-
-  private
-    def calculate_damage(target)
-      @offense - target.defense / 2
-    end
-
-    def cause_damage(**params)
-      damage = params[:damage]
-      target = params[:target]
-
-      target.hp -= damage
-      target.hp = 0 if target.hp < 0
-      puts "#{target.name} に #{damage} のダメージを与えた!"
-    end
-end
-
-class Monster < Character
-  def attack(brave)
-    # モンスターが勇者に攻撃する
-    puts "#{@name} の攻撃!"
-    # 勇者が受けるダメージの計算
-    damage = calculate_damage(brave)
-    cause_damage(target: brave, damage: damage)
-  end
-
-  private
-    def calculate_damage(target)
-      @offense - target.defense / 2
-    end
-
-    def cause_damage(**params)
-      damage = params[:damage]
-      target = params[:target]
-
-      target.hp -= damage
-      target.hp = 0 if target.hp < 0
-      puts "#{target.name} に #{damage} のダメージを与えた!"
-    end
-end
+require "./brave"
+require "./monster"
 
 
 # 実体を生成する部分、呼び出し部分 -------------------
