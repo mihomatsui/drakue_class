eba9a056e6d8ea820908965dddc02fc3593de8f3
mihomatsui <mihomatsui.24@gmail.com>
2021-01-24 11:08:00 +0900

    シドーをコメントに変更

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require './monster'
 #インスタンスを生成する
   brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
   monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
-  monster = Monster.new(name: "シドー", hp: 250, offense: 255, defense: 255)
+  # monster = Monster.new(name: "シドー", hp: 250, offense: 255, defense: 255)
 
 puts "#{monster.name} があらわれた!"
 
