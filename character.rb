ed3858d4dfc38ace4825b956e5d232910e546b3c
mihomatsui <mihomatsui.24@gmail.com>
2021-01-25 12:48:49 +0900

    メソッドの前にprivateの文言を追加

--- character.rb
+++ character.rb
@@ -9,16 +9,17 @@ class Character
     @defense = params[:defense]
   end
 
-  def calculate_damage(target)
-    @offense - target.defense / 2
-  end
+  private
+    def calculate_damage(target)
+      @offense - target.defense / 2
+    end
 
-  def cause_damage(**params)
-    damage = params[:damage]
-    target = params[:target]
+    def cause_damage(**params)
+      damage = params[:damage]
+      target = params[:target]
 
-    target.hp -= damage
-    target.hp = 0 if target.hp < 0
-    puts "#{target.name} に #{damage} のダメージを与えた!"
-  end
+      target.hp -= damage
+      target.hp = 0 if target.hp < 0
+      puts "#{target.name} に #{damage} のダメージを与えた!"
+    end
 end
